e248d070a9631aef6fcf32608fa5535659c60806
msenyk <msenyk@corevalue.net>
2016-05-28 18:34:11 +0300

    Added View permission view with route

--- js/views/Home.jsx
+++ js/views/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Home Page</div>
+    return (
+    	<div>
+    		<h1>Permission Analyzer</h1>
+    		<ul role="nav">
+    			<li><Link to="/view">View</Link></li>
+    		</ul>
+    	</div>
+	)
   }
 })
---
+++ js/views/View.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+    	<div>
+	    	<h1>View Permissions</h1>
+	    	<h2>Choose profile or permission set:</h2>
+	    	<p>TODO</p>
+    	</div>
+	)
+  }
+})
